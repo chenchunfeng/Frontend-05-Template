e8da25203f4588e9bd5aec2cd3e4686a0074ef41
chencf10 <chencf10@meicloud.com>
2021-02-03 10:08:17 +0800

    weeb-17 yeoman-vue-cli

---
+++ Week 17/yeoman-vue/webpack.config.js
@@ -0,0 +1,31 @@
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+    mode: "development", // "production" | "development" | "none"
+    entry: "./src/main.js", // string | object | array
+    module: {
+        // 模块配置相关
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader'
+                ]
+            },
+        ],
+    },
+    plugins: [
+        new VueLoaderPlugin(),
+        new CopyPlugin({
+            patterns: [
+                { from: "./src/*.html", to: '[name].[ext]' },
+            ],
+        }),
+    ],
+};
